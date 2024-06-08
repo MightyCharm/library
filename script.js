b906bd4c95d5bc16f6f2b77f6c0f29dc430aafd9
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-06-08 04:32:41 +0200

    Edit index.html, script.js and styles.css

--- script.js
+++ script.js
@@ -6,19 +6,36 @@ const form = document.querySelector("#form");
 const inputAuthor = document.querySelector("#author");
 const inputTitle = document.querySelector("#title");
 const inputPages = document.querySelector("#pages");
-const radioButtons = document.querySelectorAll("#radio");
+const radioButtons = document.querySelectorAll(".radio");
+const radioFalse = document.querySelector("#radioFalse");
 const myLibrary = [];
+
+const bgNormal= "rgba(255, 255, 255, 1)"
+const bgInvalid = "rgba(250, 50, 10, 0.6)";
 form.classList.add("hidden"); // make form invisible
 
+function resetColors() {
+    inputAuthor.style.backgroundColor = bgNormal;
+    inputTitle.style.backgroundColor = bgNormal;
+    inputPages.style.backgroundColor = bgNormal;
+}
 
-
+function resetInput() {
+    inputAuthor.value = "";
+    inputTitle.value = "";
+    inputPages.value = "";
+}
 
 function openForm() {
     console.log("openForm()");
+    // make form visible
     form.classList.remove("hidden");
+    // disable button as long form is opens
     btnNewBook.disabled = true;
-}
+    resetColors();
+    resetInput();
 
+}
 
 function getData(e) {
     console.log("getData()");
@@ -29,56 +46,65 @@ function getData(e) {
             radioVal = btn.value;
         }
     })
- 
-    console.log(`Author: ${author.value.length}`);
-    console.log(`Title: ${title.value.length}`);
-    console.log(`Pages: ${pages.value.length}`);
-    console.log(`Radio Button Value: ${radioVal}`);
-    
     // if user entered everything, create object
-    if(author.value.length > 0 && title.value.length > 0 && pages.value.length > 0) {
+    if(author.value.length > 0 && title.value.length > 0 && pages.value.length > 0 && Number(pages.value)>0) {
         e.preventDefault()
-        console.log("form filled uuuuuh yeah");
+        // create new object
+        let obj = new Book(author.value, title.value, pages.value, radioVal);
+        console.log(obj.info());
+        // call function to push new object into an array
+        addBookToLibrary(obj);
+        createCard();
+        // hide form
         form.classList.add("hidden");
+        // make button visible again and active 
         btnNewBook.classList.remove("hidden");
         btnNewBook.disabled = false;
-    } else {
+        radioFalse.checked = true;
+    } else {  
         // something was missing in user input
-        // 1. check what is missing
-        // 2. inform user
-        console.log("Input not complete. What is missing? check length of inputs");
-        
+        resetColors();
         if(author.value.length === 0) {
             console.log("author empty");
+            inputAuthor.style.backgroundColor = bgInvalid;
         }
         if(title.value.length === 0) {
             console.log("title empty");
+            inputTitle.style.backgroundColor = bgInvalid;
         }
-        if(pages.value.length === 0) {
+        if(pages.value.length === 0 || Number(pages.value) <= 0) {
             console.log("pages empty");
+            inputPages.style.backgroundColor = bgInvalid;
         }
-
-  
-
-
-    }
-
-    
+    }     
 }
 
 
 function Book(title, author, pages, read) {
+    console.log("inside Book");
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read= read
+    this.read= read,
+    this.index = myLibrary.length;
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}, index: ${this.index}`;
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(obj) {
+    console.log("function addBookToLibrary");
+    myLibrary.push(obj)
+    myLibrary.forEach((obj) => {
+        console.log(obj);
+    }) 
+}
 
+function createCard() {
+    console.log("create card()");
+    myLibrary.forEach( (obj) => {
+        console.log(obj);
+    })
 }
 
 //console.log(theHobbit.info());
@@ -86,7 +112,6 @@ function addBookToLibrary() {
 
 btnAdd.addEventListener("click", (e) => {
     console.log("btnAdd EventListener click");
-    
     getData(e);
 });
 
